dee64f2099b71e2bd527e971a0d15ec5e34fa513
Vijay090313 <vijaypratapsingh69106@gmail.com>
2026-03-29 10:04:17 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (event) => {
+    console.log('✅ KJ Dairy: Service Worker Installed');
+});
+
+self.addEventListener('fetch', (event) => {
+    // This allows the app to load faster
+});
